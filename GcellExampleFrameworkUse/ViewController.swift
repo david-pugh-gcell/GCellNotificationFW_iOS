6f353306e9c7f5d1d6461c1b3a3cfe446058adff
david-pugh-gcell <david.pugh@gcell.com>
2016-06-24 12:59:37 +0100

    Initial Commit

---
+++ GcellExampleFrameworkUse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GcellExampleFrameworkUse
+//
+//  Created by David Pugh on 24/06/2016.
+//  Copyright © 2016 G24 Power Ltd David Pugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
