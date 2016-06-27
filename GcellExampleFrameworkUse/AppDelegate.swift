b05a8327e1076ed4d705deb2458b4c8f18c73932
david-pugh-gcell <david.pugh@gcell.com>
2016-06-27 16:18:45 +0100

    Example using GCellNotificationFW to deliver did Range and notifications from iBeacons in iOS.

--- GcellExampleFrameworkUse/AppDelegate.swift
+++ GcellExampleFrameworkUse/AppDelegate.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import GCBeaconNotificationFWv1
+import CoreLocation
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GCBeaconManagerDelegate{
 
     var window: UIWindow?
+    
+    //Declare your manager as an instance variable, that way it is not destroyed and allows delegates etc
+    var beaconManager = GCBeaconManager()
+    
+    //Change option here to see different ways of work
+    var option = 2  //Use beacon.json data and notitifcations
+                    // 2 is using code defined regions and getting standard CoreLocation didRange calls
+    
+    
+  
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Use auto notifications - reads beacons and notifcations from JSON files
+        beaconManager.debug = false
+        beaconManager.delegate = self
+        
+        switch option{
+            case 1:
+                beaconManager.autoNotify = true
+                beaconManager.useDefaultBeaconRegion = false
+                beaconManager.startScanningForBeacons()
+            break
+        case 2:
+                //set the auto notify function OFF
+                beaconManager.autoNotify = false
+                
+                
+                //Set up beacon regions
+                let uuidString = "A36AD2B5-0736-43FB-8572-63DB53886FF3"
+                let beaconIdentifier = "iBeaconModules.us"
+                let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)!
+                let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+ 
+                //Add the regions
+                beaconManager.addBeaconRegion(beaconRegion)//Leave this out to use teh default GCell UUID as the beacon region
+                
+                //Start Scanning
+                beaconManager.startScanningForBeacons()
+                
+            break
+        default:
+            break
+        }
+   
+        
         return true
     }
 
@@ -42,5 +89,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate{
+    
+    //You can either react to the list of beacons that are in the notification list and in range, or just the list of actions associated with those beacons
+    func notificationBeaconsRanged(beacons: [GCBeacon]){
+        print("Ranged the following listed beacons:")
+        for b in beacons{
+            print("\(b.major)/\(b.minor) with \(b.lastKnownRssi)dB at \(b.lastSeen)")
+        }
+        
+    }
+    func notificationActionsReceived(actions: Set<GCBeaconAction>){
+        print("Actions Received:")
+        for a in actions{
+            print("\(a.actionName)")
+        }
+    }
+    
+    func errorMessage(error: Int, errorDesc: String) {
+        print("Error! \(error) - \(errorDesc)")
+    }
+    
+    
+    
+    func beaconsRanged(manager: GCBeaconManager, beacons: [CLBeacon], region: CLBeaconRegion){
+        if option == 2{
+            print("Ranged the following beacons in region \(region):")
+            for b in beacons{
+                print("\(b.major)/\(b.minor) with \(b.rssi)dB")
+            }
+        }
+
+    }
+
+    
+    
 }
 
